be236619f82429da9b97a42b4e6fb19837f71146
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-10-09 17:08:58 -0300

    Correct model class name and create talent DAO

---
+++ src/main/java/dao/TalentDAO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dao;
+
+import model.Talent;
+
+/**
+ * @author roribeir
+ *
+ */
+public class TalentDAO extends GenericDAO<Talent> {
+
+	@Override
+	public void find(Talent entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/model/Talent.java
+++ src/main/java/model/Talent.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "talentos")
-public class talent {
+public class Talent {
 	private int id;
 	private String books;
     private String name;
