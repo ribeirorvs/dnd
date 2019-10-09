abda42d1764760a23cba1f19db79780b88598baa
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-10-09 17:00:31 -0300

    Create the structure of the DAO

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -3,10 +3,28 @@
  */
 package dao;
 
+import javax.persistence.EntityManager;
+
 /**
  * @author roribeir
  *
  */
-public class GenericDAO {
-
+public abstract class GenericDAO<Entity> {
+	
+	protected EntityManager em;
+	
+	public void insert(Entity entity) {
+		em.persist(entity);
+	}
+	
+	public void update(Entity entity) {
+		em.merge(entity);
+	}
+	
+	public void delete(Entity entity) {
+		em.remove(entity);
+	}
+	
+	public abstract void find(Entity entity);
+	
 }
