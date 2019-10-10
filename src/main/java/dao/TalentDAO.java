0641a015ce8642f8006b04f4df1d3be4436bf246
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-10-10 15:32:29 -0300

    Create find all talents method

--- src/main/java/dao/TalentDAO.java
+++ src/main/java/dao/TalentDAO.java
@@ -12,6 +12,11 @@ import model.Talent;
  *
  */
 public class TalentDAO extends GenericDAO<Talent> {
+	
+	public List<Talent> findAllTalents(){
+		return em.createNamedQuery("getAllTalents", Talent.class).getResultList();
+	}
+	
 	@Override
 	public Talent find(Talent talent) {
 		return em.find(Talent.class, talent);
